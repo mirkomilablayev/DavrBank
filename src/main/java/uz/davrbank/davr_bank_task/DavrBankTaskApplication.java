26d5c8c207eeaac129bb96fcf15720adc39aa67e
mirkomil <mirkomilablayev777@gmai;.com>
2022-08-11 14:21:58 +0500

    assasas

---
+++ src/main/java/uz/davrbank/davr_bank_task/DavrBankTaskApplication.java
@@ -0,0 +1,13 @@
+package uz.davrbank.davr_bank_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavrBankTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DavrBankTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/davrbank/davr_bank_task/DavrBankTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.davrbank.davr_bank_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavrBankTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
